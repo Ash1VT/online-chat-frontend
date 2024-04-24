e255e102321739a3196eb94dfdc25dc378ae6188
Панасевич Никита <nikita.panasevitch@yandex.by>
2024-04-24 23:33:05 +0300

    Updates

--- chat/src/configuration/constants.js
+++
@@ -1,9 +0,0 @@
-const accessTokenName = 'access'
-const refreshTokenName = 'refresh'
-
-
-const baseURL = 'localhost:8000'
-const httpBaseURL = `http://${baseURL}/`
-const wsBaseURL = `ws://${baseURL}/`
-
-export {accessTokenName, refreshTokenName, baseURL, httpBaseURL, wsBaseURL}
